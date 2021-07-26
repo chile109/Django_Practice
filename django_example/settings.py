c48962fc81490f64759d58928506dfa5c02cb604
chile109 <chile109@gmail.com>
2021-07-26 18:00:33 +0800

    use database_url for heroku

--- django_example/settings.py
+++ django_example/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,14 +76,7 @@ WSGI_APPLICATION = "django_example.wsgi.application"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "HOST": "db",
-        "PORT": 5432,
-        "PASSWORD": "postgres",
-    }
+    "default": dj_database_url.config()
 }
 
 # Password validation
